5e0451c825e149a611f8ad79452edc16bd35eb63
LAKSHYA CHOUDHARY <choudharylakshya.lc.com>
2026-09-02 01:22:56 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -35,10 +35,7 @@ app.use(cors({
 }));
 app.use(clerkMiddleware());
 
-console.log(
-  "🔥 INNGEST FUNCTIONS:",
-  functions.map((fn) => fn.id)
-);
+
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
